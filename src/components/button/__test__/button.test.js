8609ad575cc8214399b18c9a2907b2a1a0186068
leonzhang <human1108@163.com>
2021-09-14 19:22:35 +0800

    fix: remove test in components

--- src/components/button/__test__/button.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'enzyme'
-import React from 'react'
-import Button from '../index'
-
-test('render button', () => {
-  const wrapper = shallow(<Button>Hello test</Button>)
-  expect(wrapper).toMatchSnapshot()
-})
